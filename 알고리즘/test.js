56c65aaf1597b08a19865a3782012e702a37d2ac
dohpark <parkdohee19931129@gmail.com>
2021-10-04 21:42:15 +0900

    hash table

--- 알고리즘/test.js
+++ 알고리즘/test.js
@@ -1,96 +1,103 @@
-// Dictionary()
-function Dictionary(items = {}) {
-  this.items = items;
+const HASH_SIZE = 37;
+
+// Element(): key, value 저장을 위한 생성자
+function Element(key, value) {
+  this.key = key;
+  this.value = value;
 }
 
-// getBuffer()
-Dictionary.prototype.getBuffer = function () {
-  return { ...this.items }; // spread문법 사용
-};
-
-// clear()
-Dictionary.prototype.clear = function () {
-  this.items = {}; // 초기화
-};
-
-// size()
-Dictionary.prototype.size = function () {
-  return Object.keys(this.items).length; // key값으로 구성한 배열로 변환 후 length 값 반환
-};
-
-// has(): 개체 존재 여부 확인
-Dictionary.prototype.has = function (key) {
-  // return value in this.items; <- 배열로 존재하는지 확인
-  return this.items.hasOwnProperty(key);
-  // Object.hasOwnProperty: 객체 내 특정 key 값이 존재하면 true 반환. key의 value값이 null or undefined이어도 true 반환
-};
-
-// set(): 개체(Entity) 추가
-Dictionary.prototype.set = function (key, value) {
-  this.items[key] = value;
-};
-
-// get(): 개체(Entity)의 value 반환
-Dictionary.prototype.get = function (key) {
-  return this.has(key) ? this.items[key] : undefined; // 있는지 없는지 확인 후 있으면 반환
-};
-
-// remove(): 개체 (Entity) 삭제
-Dictionary.prototype.remove = function (key) {
-  if (this.has(key)) {
-    // key가 있는지 없는지 확인 후
-    delete this.items[key]; // 삭제
-    return true;
-  }
-
-  return false;
-};
-
-// keys(): 모든 key 값을 배열 형태로 반환
-Dictionary.prototype.keys = function () {
-  return Object.keys(this.items);
-};
-
-// values(): 모든 value 값을 배열 형태로 반환
-Dictionary.prototype.values = function () {
-  /**
-   * let values = [];
-   * for (let k in this.items) {
-   *   values.push(this.items[k]);
-   * }
-   * return values;
-   */
-
-  return Object.values(this.items);
-};
-
-// each()
-Dictionary.prototype.each = function (fn) {
-  for (let k in this.items) {
-    fn(k, this.items[k]);
-  }
-};
-
-// printDictionary()
-function printDictionary(key, value) {
-  console.log(`key: ${key}`);
-  console.log(`value: ${value}`);
+// HashTable(): 생성자
+function HashTable() {
+  this.table = new Array(HASH_SIZE);
+  this.length = 0;
 }
 
-let dict = new Dictionary();
-dict.set("age", 19);
-dict.set("name", "alice");
-dict.set("height", 172);
-console.log(dict);
+// hashCode(): 해시 함수
+HashTable.prototype.hashCode = function (key) {
+  let hash = 0;
+  for (let i = 0; i < key.length; i++) {
+    hash += key.charCodeAt(i);
+  }
+  return hash % HASH_SIZE;
+};
 
-dict.remove("age");
-console.log(dict);
+// put(): 데이터 추가
+HashTable.prototype.put = function (key, value) {
+  let index = this.hashCode(key);
+  console.log(`key: ${key} -> index: ${index}`); // debug용
 
-console.log(dict.has("age"));
-console.log(dict.has("name"));
-console.log(dict.get("age"));
-console.log(dict.get("name"));
+  if (this.table[index] !== undefined) {
+    return false; // 이미 값이 있는 경우
+  }
 
-console.log(dict.keys());
-console.log(dict.values());
-dict.each(printDictionary);
+  // 아직 없으며 해시 테이블에 값 없으면 저장
+  this.table[index] = new Element(key, value);
+  this.length++;
+
+  return true;
+};
+
+// get(): 데이터 조회
+HashTable.prototype.get = function (key) {
+  return this.table[this.hashCode(key)];
+};
+
+// remove(): 데이터 삭제
+HashTable.prototype.remove = function (key) {
+  let element = this.table[this.hashCode(key)]; // 해당 해시 테이블에 값 있는지 없는지 확인. 없으면 undefined 저장
+
+  // 값 있으면 삭제
+  if (element !== undefined) {
+    delete this.table[this.hashCode(key)];
+    this.length--;
+  }
+  // 값 없으면 undefined 반환
+  return element;
+};
+
+// clear(): 초기화
+HashTable.prototype.clear = function () {
+  this.table = new Array(HASH_SIZE);
+  this.length = 0;
+};
+
+// size(): 크기 반환
+HashTable.prototype.size = function () {
+  return this.length;
+};
+
+// getBuffer(): 데이터 셋 반환
+HashTable.prototype.getBuffer = function () {
+  let array = [];
+  for (let i = 0; i < this.table.length; i++) {
+    if (this.table[i]) {
+      // 값이 있을 때만 반환
+      array.push(this.table[i]);
+    }
+  }
+  return array;
+};
+
+// print(): 데이터 셋 출력
+HashTable.prototype.print = function () {
+  for (let i = 0; i < this.table.length; i++) {
+    if (this.table[i]) {
+      // 값이 있을 때만 key값 value값 프린트
+      console.log(i + " -> " + this.table[i].key + ": " + this.table[i].value);
+    }
+  }
+};
+
+let ht = new HashTable();
+
+ht.put("Ana", 172);
+ht.put("Sue", 163);
+ht.put("Paul", 190);
+
+console.log("");
+ht.print();
+console.log(ht.getBuffer());
+
+console.log(ht.size());
+ht.clear();
+console.log(ht);
