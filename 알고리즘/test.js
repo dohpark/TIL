684ff9968dfcfbef79ed8d773c75ed483284daa7
dohpark <parkdohee19931129@gmail.com>
2021-10-05 21:56:56 +0900

    선형조사법 해쉬테이블 구현

--- 알고리즘/test.js
+++ 알고리즘/test.js
@@ -1,4 +1,4 @@
-const HASH_SIZE = 37;
+const HASH_SIZE = 5; // 충돌 늘리기 위해 작게 함
 
 // Element(): key, value 저장을 위한 생성자
 function Element(key, value) {
@@ -6,68 +6,34 @@ function Element(key, value) {
   this.value = value;
 }
 
-// HashTable(): 생성자
-function HashTable() {
+// LinearHashTable(): 생성자
+function LinearHashTable() {
   this.table = new Array(HASH_SIZE);
   this.length = 0;
 }
 
 // hashCode(): 해시 함수
-HashTable.prototype.hashCode = function (key) {
+LinearHashTable.prototype.hashCode = function (key) {
   let hash = 0;
   for (let i = 0; i < key.length; i++) {
     hash += key.charCodeAt(i);
   }
   return hash % HASH_SIZE;
-};
-
-// put(): 데이터 추가
-HashTable.prototype.put = function (key, value) {
-  let index = this.hashCode(key);
-  console.log(`key: ${key} -> index: ${index}`); // debug용
-
-  if (this.table[index] !== undefined) {
-    return false; // 이미 값이 있는 경우
-  }
-
-  // 아직 없으며 해시 테이블에 값 없으면 저장
-  this.table[index] = new Element(key, value);
-  this.length++;
-
-  return true;
-};
-
-// get(): 데이터 조회
-HashTable.prototype.get = function (key) {
-  return this.table[this.hashCode(key)];
-};
-
-// remove(): 데이터 삭제
-HashTable.prototype.remove = function (key) {
-  let element = this.table[this.hashCode(key)]; // 해당 해시 테이블에 값 있는지 없는지 확인. 없으면 undefined 저장
-
-  // 값 있으면 삭제
-  if (element !== undefined) {
-    delete this.table[this.hashCode(key)];
-    this.length--;
-  }
-  // 값 없으면 undefined 반환
-  return element;
-};
+}; // loselosehash 알고리즘
 
 // clear(): 초기화
-HashTable.prototype.clear = function () {
+LinearHashTable.prototype.clear = function () {
   this.table = new Array(HASH_SIZE);
   this.length = 0;
 };
 
 // size(): 크기 반환
-HashTable.prototype.size = function () {
+LinearHashTable.prototype.size = function () {
   return this.length;
 };
 
 // getBuffer(): 데이터 셋 반환
-HashTable.prototype.getBuffer = function () {
+LinearHashTable.prototype.getBuffer = function () {
   let array = [];
   for (let i = 0; i < this.table.length; i++) {
     if (this.table[i]) {
@@ -79,7 +45,7 @@ HashTable.prototype.getBuffer = function () {
 };
 
 // print(): 데이터 셋 출력
-HashTable.prototype.print = function () {
+LinearHashTable.prototype.print = function () {
   for (let i = 0; i < this.table.length; i++) {
     if (this.table[i]) {
       // 값이 있을 때만 key값 value값 프린트
@@ -88,16 +54,78 @@ HashTable.prototype.print = function () {
   }
 };
 
-let ht = new HashTable();
+// put(): 데이터 추가
+LinearHashTable.prototype.put = function (key, value) {
+  let index = this.hashCode(key);
+  let startIndex = index;
+  console.log(`key: ${key} -> index: ${index}`);
 
-ht.put("Ana", 172);
-ht.put("Sue", 163);
-ht.put("Paul", 190);
+  do {
+    if (this.table[index] === undefined) {
+      // 값 존재 하지 않다면
+      this.table[index] = new Element(key, value); // 삽입
+      this.length++;
+      return true;
+    }
 
+    index = (index + 1) % HASH_SIZE; // modular 연산
+  } while (index !== startIndex); // 만약에 한 바퀴 돌아 처음 인덱스로 돌아오면 while문 벗어나 false 반환
+
+  return false;
+};
+
+// get(): 데이터 조회
+LinearHashTable.prototype.get = function (key) {
+  let index = this.hashCode(key);
+  let startIndex = index;
+
+  do {
+    // 값 있는지 확인도 하고, 해당 key가 같은지도 확인함
+    if (this.table[index] !== undefined && this.table[index].key === key) {
+      return this.table[index].value;
+    }
+
+    index = (index + 1) % HASH_SIZE;
+  } while (index !== startIndex);
+
+  return undefined;
+};
+
+// remove(): 데이터 삭제
+LinearHashTable.prototype.remove = function (key) {
+  let index = this.hashCode(key);
+  let startIndex = index;
+
+  do {
+    if (this.table[index] !== undefined && this.table[index].key === key) {
+      let element = this.table[index];
+      delete this.table[index];
+      this.length--;
+
+      return element;
+    }
+
+    index = (index + 1) % HASH_SIZE;
+  } while (index !== startIndex);
+
+  return undefined;
+};
+
+let lht = new LinearHashTable();
+
+lht.put("Ana", 172);
+lht.put("John", 179);
+lht.put("Donnie", 183);
+lht.put("Mindy", 190);
+lht.put("Paul", 168);
 console.log("");
-ht.print();
-console.log(ht.getBuffer());
 
-console.log(ht.size());
-ht.clear();
-console.log(ht);
+console.log(lht.remove("Ana"));
+console.log(lht.get("Paul"));
+console.log(lht.remove("Paul"));
+console.log(lht.get("Paul"));
+console.log(lht.size());
+console.log("");
+
+lht.print();
+console.log(lht);
