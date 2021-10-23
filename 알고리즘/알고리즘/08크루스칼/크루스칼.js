acf89fb499f2d960a9076d8b5bd21c0b5a5a6720
dohpark <parkdohee19931129@gmail.com>
2021-10-23 23:02:50 +0900

    크루스칼

---
+++ 알고리즘/알고리즘/08크루스칼/크루스칼.js
@@ -0,0 +1,67 @@
+function Graph() {
+  this.edges = [];
+}
+
+// addEdge(): 간선 추가
+Graph.prototype.addEdge = function (srcVertex, dstVertex, weight) {
+  this.edges.push([srcVertex, dstVertex, weight]);
+};
+
+// union find algorithm
+Graph.prototype.getParent = function (parent, x) {
+  if (parent[x] == x) return x;
+  return (parent[x] = this.getParent(parent, parent[x]));
+};
+
+Graph.prototype.unionParent = function (parent, a, b) {
+  const n1 = this.getParent(parent, a);
+  const n2 = this.getParent(parent, b);
+  if (n1 < n2) return (parent[n2] = n1);
+  else return (parent[n1] = n2);
+};
+
+Graph.prototype.findParent = function (parent, a, b) {
+  const n1 = this.getParent(parent, a);
+  const n2 = this.getParent(parent, b);
+  if (n1 === n2) return true;
+  else return false;
+};
+
+// kruskal
+Graph.prototype.kruskal = function () {
+  let answer = 0;
+  let parent = [];
+
+  let vertex = new Set();
+  for (let edge of this.edges) {
+    vertex.add(edge[0]);
+    vertex.add(edge[1]);
+  }
+
+  parent = [...vertex];
+
+  let costs = this.edges;
+  costs.sort((a, b) => a[2] - b[2]);
+
+  console.log("parent", parent);
+  for (let cost of costs) {
+    console.log("cost", cost);
+    if (!this.findParent(parent, cost[0], cost[1])) {
+      answer += cost[2];
+      this.unionParent(parent, cost[0], cost[1]);
+    }
+    console.log("parent", parent);
+  }
+  return answer;
+};
+
+let graph = new Graph();
+
+graph.addEdge(0, 1, 1);
+graph.addEdge(0, 2, 2);
+graph.addEdge(1, 2, 5);
+graph.addEdge(1, 3, 1);
+graph.addEdge(2, 3, 8);
+
+console.log(graph);
+console.log(graph.kruskal());
