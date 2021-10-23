325677331cf82b0dae2c0c22b01ce985293ed160
dohpark <parkdohee19931129@gmail.com>
2021-10-23 23:00:32 +0900

    크루스칼

--- 알고리즘/test.js
+++ 알고리즘/test.js
@@ -1,94 +1,69 @@
-// TrieNode(): 문자 노드와 끝 단어 표시를 위한 노드 생성자
-function TrieNode() {
-  this.children = {}; // key: 문자, value: TrieNode
-  this.endOfWord = false; // 단어 여부
+// ShortestPath(): edge object 객체 저장을 위한 생성자
+function Graph() {
+  this.edges = [];
 }
 
-// Trie(): 루트 노드 저장을 위한 생성자
-function Trie() {
-  this.root = new TrieNode();
-}
+// addEdge(): 간선 추가
+Graph.prototype.addEdge = function (srcVertex, dstVertex, weight) {
+  this.edges.push([srcVertex, dstVertex, weight]);
+};
 
-// insert(): 문자열 추가
-Trie.prototype.insert = function (word) {
-  let current = this.root; // root부터 시작
+// union find algorithm
+Graph.prototype.getParent = function (parent, x) {
+  if (parent[x] == x) return x;
+  return (parent[x] = this.getParent(parent, parent[x]));
+};
 
-  for (let i = 0; i < word.length; i++) {
-    let ch = word[i]; // 각 character 하나씩 검색할 예정
-    let node = current.children[ch]; // 해당 character을 키로 지닌 child 노드를 찾음
+Graph.prototype.unionParent = function (parent, a, b) {
+  const n1 = this.getParent(parent, a);
+  const n2 = this.getParent(parent, b);
+  if (n1 < n2) return (parent[n2] = n1);
+  else return (parent[n1] = n2);
+};
 
-    // 만약 찾는 child 노드가 없으면
-    if (node === undefined) {
-      node = new TrieNode(); // 노드 신규 생성
-      current.children[ch] = node; // 신규 노드를 현재 위치의 child 노드에 저장
+Graph.prototype.findParent = function (parent, a, b) {
+  const n1 = this.getParent(parent, a);
+  const n2 = this.getParent(parent, b);
+  if (n1 === n2) return true;
+  else return false;
+};
+
+// kruskal
+Graph.prototype.kruskal = function () {
+  let answer = 0;
+  let parent = [];
+
+  let vertex = new Set();
+  for (let edge of this.edges) {
+    vertex.add(edge[0]);
+    vertex.add(edge[1]);
+  }
+
+  parent = [...vertex];
+
+  let costs = this.edges;
+  costs.sort((a, b) => a[2] - b[2]);
+
+  console.log("parent", parent);
+  for (let cost of costs) {
+    console.log("cost", cost);
+    // 이미 이어져있는 정점에 새로 간선을 추가할려고 하면 사이클이 형성되기에 findParent의 값이 false인 값들만 추가시킴
+    if (!this.findParent(parent, cost[0], cost[1])) {
+      answer += cost[2];
+      this.unionParent(parent, cost[0], cost[1]);
     }
-
-    current = node; // 현재 위치를 해당 child 노드로 진입
+    console.log("parent", parent);
   }
-
-  current.endOfWord = true; // 만약 한 단어의 끝이라면 끝이라는 표시를 남김
+  return answer;
 };
 
-// search(): 문자열 검색
-// insert()과 원리 매우 유사
-Trie.prototype.search = function (word) {
-  let current = this.root;
+let graph = new Graph();
 
-  for (let i = 0; i < word.length; i++) {
-    let ch = word[i];
-    let node = current.children[ch];
+graph.addEdge(0, 1, 1);
+graph.addEdge(0, 2, 2);
+graph.addEdge(1, 2, 5);
+graph.addEdge(1, 3, 1);
+graph.addEdge(2, 3, 8);
 
-    if (node === undefined) {
-      return false; // 만약에 비어 있으면 해당 단어가 없다는 의미로 false 반환
-    }
-
-    current = node;
-  }
-
-  return current.endOfWord;
-};
-// 해당 노드의 children들을 찾으며 타고 내려가 결과적으로 원하는 단어를 찾는 구조
-
-// delete(): 문자열 삭제
-Trie.prototype.delete = function (word, current = this.root, index = 0) {
-  if (index === word.length) {
-    if (!current.endOfWord) return false;
-
-    current.endOfWord = false;
-
-    return Object.keys(current.children).length === 0;
-  }
-
-  let ch = word[index];
-  let node = current.children[ch];
-
-  if (node === undefined) return false;
-
-  let isDeleteNode = this.delete(word, node, index + 1) && !node.endOfWord;
-  if (isDeleteNode) {
-    delete current.children[ch];
-    return Object.keys(current.children).length === 0;
-  }
-
-  return false;
-};
-
-let trie = new Trie();
-
-trie.insert("be");
-trie.insert("bee");
-trie.insert("can");
-trie.insert("cat");
-trie.insert("cd");
-
-console.log(trie.search("bee"));
-trie.delete("bear");
-console.log(trie.search("bee"));
-trie.delete("b");
-console.log(trie.search("bee"));
-trie.delete("bee");
-console.log(trie.search("bee"));
-
-console.log(trie.root.children);
-console.log(trie.root.children["b"]);
-console.log(trie.root.children["b"].children["e"]);
+console.log(graph);
+console.log(graph.kruskal());
